61633c70e1e4825f0afa94bb01a538a8caf954f5
Khalid Khalil <coderizer@aol.com>
2020-11-09 20:19:17 +0200

    Add API endpoints for listing/deleting shipments

---
+++ src/views/Blends/resources/promocodes/ListView.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+import Breadcrumbs from "../../../../components/@vuexy/breadCrumbs/BreadCrumb";
+import ListViewConfig from "./DataListConfig";
+import queryString from "query-string";
+class ListView extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Breadcrumbs
+          breadCrumbTitle="Promocodes"
+          breadCrumbParent="Offers & Promocodes"
+          breadCrumbActive="Promocodes List"
+        />
+        <Row>
+          <Col sm="12">
+            <ListViewConfig
+              parsedFilter={queryString.parse(this.props.location.search)}
+            />
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ListView;
